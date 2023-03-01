726de864b0213a1d7ccc31635e28c65aa23010d7
keszod <52524513+keszod@users.noreply.github.com>
2023-03-01 08:46:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,62 +7,22 @@ import csv
 import traceback
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver as uc
 
 from time import sleep
 from bs4 import BeautifulSoup as bs
 
 def create_driver(headless=True):
-	print('create_driver()')
-	chrome_options = webdriver.ChromeOptions()
-	if headless:
-		chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--log-level=3")
-	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36')
-	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-	#chrome_options.add_argument('--proxy-server='+proxy)
+	options = uc.ChromeOptions()
+	options.headless=headless
+	options.add_argument("--window-size=1920,1080")
+	options.add_argument('--ignore-certificate-errors')
+	options.add_argument('--allow-running-insecure-content')
+	options.add_argument('--headless')
 
-	chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-	chrome_options.add_experimental_option('useAutomationExtension', False)
-	chrome_options.add_argument("--disable-blink-features")
-	chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36")
-	chrome_options.add_experimental_option("prefs", { 
-	"profile.default_content_setting_values.media_stream_mic": 1, 
-	"profile.default_content_setting_values.media_stream_camera": 1,
-	"profile.default_content_setting_values.geolocation": 1, 
-	"profile.default_content_setting_values.notifications": 1,
-	"profile.default_content_settings.geolocation": 1,
-	"profile.default_content_settings.popups": 0
-  })
-	
-	caps = DesiredCapabilities().CHROME
+	driver = uc.Chrome(use_subprocess=True, options=options)
 
-	caps["pageLoadStrategy"] = "none"	
-	
-	driver = webdriver.Chrome(desired_capabilities=caps,chrome_options=chrome_options)	
-
-	driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
- "source": """
-	  const newProto = navigator.__proto__
-	  delete newProto.webdriver
-	  navigator.__proto__ = newProto		
-	  """
-})
-	driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-		
-	driver.implicitly_wait(10)
-
-	params = {
-	"latitude": 55.5815245,
-	"longitude": 36.825144,
-	"accuracy": 100
-	}
-	driver.execute_cdp_cmd("Emulation.setGeolocationOverride", params)
-	driver.refresh()
-	driver.set_page_load_timeout(10)
-	
 	return driver
-
 
 def get_sites():
 	with open('sites.txt','r',encoding='utf-8-sig') as file:
@@ -131,7 +91,8 @@ def check_if_news():
 			traceback.print_exc()
 			continue
 
-#sort_data()
-if not os.path.exists('result.csv'):
-	csv_pack('result',['Сайт',"Кол-во ссылок","Кол-во дат","Если слово news в тексте","Последний указаный год"],'w')
-check_if_news()
+if __name__ == '__main__':
+	#sort_data()
+	if not os.path.exists('result.csv'):
+		csv_pack('result',['Сайт',"Кол-во ссылок","Кол-во дат","Если слово news в тексте","Последний указаный год"],'w')
+	check_if_news()
